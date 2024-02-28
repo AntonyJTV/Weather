a7c5dea5b122a5090c3592e5dbe62974b7024518
AntonyJTV <92594594+AntonyJTV@users.noreply.github.com>
2024-02-28 07:53:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ search.addEventListener('click', () =>{
                 image.src = 'images/cloud.png';
                 break;
             
-            case 'Haze':
+            case 'Mist':
                 image.src = 'images/mist.png';
                 break;
             
